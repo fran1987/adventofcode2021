a24cc00cc1859e4e64097cdd79bf9dac747b0297
Fran Tonković <ftonkovic@fer.hr>
2021-12-13 12:59:57 +0100

    replace XXX in template

--- newDay.js
+++ newDay.js
@@ -1,4 +1,4 @@
-import { mkdirSync,writeFileSync,existsSync, copyFileSync } from 'fs';
+import { mkdirSync,writeFileSync,existsSync, copyFileSync, readFileSync } from 'fs';
 import { exit } from 'process';
 import axios from 'axios';
 import dotenv from 'dotenv';
@@ -12,8 +12,11 @@ let dayDirectory = day.padStart(2, '0');
 if(existsSync(dayDirectory)) exit(1);
 
 mkdirSync(dayDirectory);
-copyFileSync('template.js', `${dayDirectory}/index.js`,)
-// writeFileSync(`${dayDirectory}/index.js`,'');
+
+//copyFileSync('template.js', `${dayDirectory}/index.js`,);
+let template = readFileSync('template.js', 'utf-8');
+writeFileSync(`${dayDirectory}/index.js`,template.replace(/XXX/g, day));
+
 writeFileSync(`${dayDirectory}/testInput.txt`,'');
 
 axios.get(`https://adventofcode.com/2021/day/${day}/input`,{
