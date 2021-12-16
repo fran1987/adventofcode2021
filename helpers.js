af273bee5fe20a15a4138fad0555d27501822ec3
Fran Tonković <ftonkovic@fer.hr>
2021-12-16 12:03:37 +0100

    input parsing additions

--- helpers.js
+++ helpers.js
@@ -1,17 +1,18 @@
 import { readFileSync } from 'fs';
 
-export function getInput(day, test) {
+export function getInput(day, test, rowSplitDelimiter, mapFunction) {
 
     let input = [];
     let directory = ''
     let fileName = 'input.txt'
-    if(test) fileName = 'testInput.txt';
+    if (test) fileName = 'testInput.txt';
     if (day) {
         directory = `${String(day).padStart(2, '0')}/`;
     }
     let path = `${directory}${fileName}`;
     readFileSync(path, 'utf-8').split(/\r?\n/).forEach(function (line) {
-        input.push(line);
+        if (rowSplitDelimiter != null && mapFunction != null) input.push(line.split(rowSplitDelimiter).map(mapFunction));
+        else input.push(line);
     });
     return input;
 }
